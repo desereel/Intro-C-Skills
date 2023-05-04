fc552d50e435c2e3f8465c59f08f9bbf13456c1a
Deseree Frink-Larcart <dfrinkla@nd.edu>
2023-05-04 10:51:51 -0400

    Small Newb Codes

---
+++ hello.c
@@ -0,0 +1,23 @@
+/**********************************************
+* File: hello.c
+* Author: Deseree Frink-Larcart
+* Email: dfrinkla@nd.edu
+* 
+* This is the classic Hello, World program 
+**********************************************/
+
+#include <stdio.h> /* fprintf */
+
+/********************************************
+* Function Name  : main
+* Pre-conditions : void
+* Post-conditions: int
+* 
+* This is the main driver function for the program 
+********************************************/
+int main(void){
+	
+	fprintf(stdout, "Look at my coding skills!\n");
+
+	return 0;
+}
